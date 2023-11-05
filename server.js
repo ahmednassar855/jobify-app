e0d8824bcb0b9d297e96ac9b26de8db05c137db3
ahmednassar855 <ahmed.nassar855@yahoo.com>
2023-11-05 21:41:43 +0200

    preparation production v8

--- server.js
+++ server.js
@@ -30,14 +30,13 @@ cloudinary.config({
 });
 
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
-
+const __dirname = dirname(fileURLToPath(import.meta.url))
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
 
-app.use(express.static(path.resolve(__dirname, 'client' ,'dist')));
+app.use(express.static(path.resolve(__dirname, './client/dist')));
 app.use(cookieParser());
 app.use(express.json())
 
@@ -53,9 +52,8 @@ app.use('/api/v1/users', authenticateUser, userRouter);
 app.use('/api/v1/auth', authRouter);
 
 
-
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'client' ,'dist', 'index.html'));
+  res.sendFile(path.resolve(__dirname, './client/dist', 'index.html'));
 });
 
 app.use('*', (req, res) => {
