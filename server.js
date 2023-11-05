1d2149cb807809a9fb9e5c65e572ffb527a72e05
ahmednassar855 <ahmed.nassar855@yahoo.com>
2023-11-05 21:27:13 +0200

    preparation production v7

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 
-app.use(express.static(path.resolve(__dirname, './client/dist')));
+app.use(express.static(path.resolve(__dirname, 'client' ,'dist')));
 app.use(cookieParser());
 app.use(express.json())
 
